ca3478692c6cb04530fd6232f3eebe7b6bafeec8
Turupawn <ahmed.hn.43@gmail.com>
2016-06-18 16:48:54 -0600

    cleanning

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // server.js
 // load the things we need
 var express = require('express');
-var requesta = require("request")
+var request = require("request")
 var app = express();
 
 // set the view engine to ejs
@@ -14,31 +14,16 @@ app.get('/', function(req, res) {
   access_token = req.url.split('value=')[1]
   if(access_token)
   {
-    requesta(
+    request(
     {
       url: 'https://api.thingiverse.com/collections/5907119/things?access_token='+access_token,
       json: true
     }, function (error, response_thingiverse, body)
     {
-      str_response = "<h1>Rosalila's Hub History</h1>"
-      for(i=0;i<JSON.stringify(response_thingiverse.body.length);i++)
-      {
-        str_response+= "<h2>"+response_thingiverse.body[i].name+"</h2>"
-        str_response+= "<img src=\'"+response_thingiverse.body[i].thumbnail+"\'/>"
-        str_response+= "<p>"+JSON.stringify(response_thingiverse.body[i])+"</p>"
-      }
-      //response.end(str_response);
-
-    res.render('pages/index',{not_logged:false,thingiverse:response_thingiverse.body});
+      res.render('pages/index',{not_logged:false,thingiverse:response_thingiverse.body});
     })
   }else
   {
-    str_response ="<a href='https://www.thingiverse.com/login/oauth/authorize?client_id=01e6eab25045aa343883&redirect_uri=http://rosalilastudio.com:8000&response_type=token'>login with thingiverse</a>"
-    str_response+= "<script>"
-    str_response+= "if(window.location.hash.split('access_token=')[1])"
-    str_response+= "window.location = '/?value='+window.location.hash.split('access_token=')[1]"
-    str_response+= "</script>"
-    //response.end(str_response);
     res.render('pages/index',{not_logged:true});
   }
 });
